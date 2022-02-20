3a323153d6c102a5e6dcd6ffc274c50bcbc76ebc
Khadija Rahman <khadija.rahman@and.digital>
2022-02-20 19:19:05 +0000

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// entry point where does out application kick off
+const path = require("path");
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: "/Users/khadijarahman/Documents/code/indecision-app/public",
+    filename: "bundle.js",
+  },
+};
+
+//where to output the final bundle file

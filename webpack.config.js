92666e198938476d61cdfee6d307d3de43c282ce
Khadija Rahman <khadija.rahman@and.digital>
2022-02-25 10:54:25 +0000

    add github pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/app.js",
   output: {
-    path: path.join(__dirname, "/"),
+    path: path.join(__dirname, "public"),
     filename: "bundle.js",
   },
   module: {
